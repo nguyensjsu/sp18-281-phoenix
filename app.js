6372a83ff7f97dbc30c170b49f616fa1638b8785
Animesh Grover <animesh.grover@sjsu.edu>
2018-04-06 15:27:13 -0700

    added session storage to nodejs app

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ Version 1.0
 var fs = require('fs');
 var express = require('express');
 var Client = require('node-rest-client').Client;
+var session = require('client-sessions');
 
 var app = express();
 app.use(express.bodyParser());
@@ -33,6 +34,20 @@ var page = function( req, res, location ) {
     }
     res.end( body );
 }
+
+app.use(session({
+  cookieName: 'session',
+  secret: 'myStore',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
+
+app.post('/setStore', function(req, res) {
+  req.session.store = req.body.store;
+  res.cookie('storeLocation',req.body.store, { maxAge: 900000, httpOnly: true });
+  console.log('cookie created successfully');
+  res.redirect('/locations.html');
+});
 
 var handle_get = function (req, res) {
     page( req, res, "San Jose" ) ;
