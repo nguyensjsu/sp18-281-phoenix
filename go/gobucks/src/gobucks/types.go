357e00ac050722532df0c934c30a91cbfc416f93
Animesh Grover <animesh70@gmail.com>
2018-04-27 23:22:00 -0700

    updated postman collection and modified order and item struct

--- go/gobucks/src/gobucks/types.go
+++ go/gobucks/src/gobucks/types.go
@@ -6,11 +6,17 @@ import (
 
 type order struct {
 	Id						string
-	OrderName     string
+	Items     []item
+	TotalPrice        	string
+	OrderStatus 	string
+}
+
+type item struct {
+	Drink			string
 	Size					string
 	Options				string
 	Price        	string
-	OrderStatus 	string
+	Quantity     int
 }
 
 var client *redis.Client
